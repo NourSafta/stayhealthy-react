ba486b9b2a5bdce1d1f83f96ce21b6d3525e97c8
NourSafta <nour.safta@medtech.tn>
2025-06-16 16:47:30 -0400

    Update MongoDB connection with latest credentials

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:nCobFWgsTcNZsBeCgO0IhLEF@172.21.191.10:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
