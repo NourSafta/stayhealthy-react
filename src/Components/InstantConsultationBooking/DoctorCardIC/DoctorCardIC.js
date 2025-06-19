835d972a7b399988d6c2611c1b105d97a76afadd
NourSafta <nour.safta@medtech.tn>
2025-06-18 20:35:12 -0400

    Add individual doctor images and updated styling

--- src/Components/InstantConsultationBooking/DoctorCardIC/DoctorCardIC.js
+++ src/Components/InstantConsultationBooking/DoctorCardIC/DoctorCardIC.js
@@ -30,11 +30,14 @@ const DoctorCardIC = ({ name, speciality, experience, ratings }) => {
   return (
     <div className="doctor-card-container">
       <div className="doctor-card-details-container">
-        <div className="doctor-card-profile-image-container">
-          <svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" fill="currentColor" className="bi bi-person-fill" viewBox="0 0 16 16">
-            <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
-          </svg>
-        </div>
+      <div className="doctor-card-profile-image-container">
+        <img
+            src={`${process.env.PUBLIC_URL}/doctor1.jpg`}
+            alt="Doctor"
+            className="doctor-card-profile-img"
+        />
+      </div>
+
         <div className="doctor-card-details">
           <div className="doctor-card-detail-name">{name}</div>
           <div className="doctor-card-detail-speciality">{speciality}</div>
@@ -61,9 +64,11 @@ const DoctorCardIC = ({ name, speciality, experience, ratings }) => {
           {(close) => (
             <div className="doctorbg" style={{ height: '100vh', overflowY: 'auto' }}>
               <div className="doctor-card-profile-image-container">
-                <svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" fill="currentColor" className="bi bi-person-fill" viewBox="0 0 16 16">
-                  <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
-                </svg>
+              <img
+                src={`${process.env.PUBLIC_URL}/doctor1.jpg`}
+                alt="Doctor"
+                className="doctor-card-profile-img"
+              />
               </div>
               <div className="doctor-card-details">
                 <div className="doctor-card-detail-name">{name}</div>
