213f9d201ea2941c78d793da62d8ddf3f1032bdb
NourSafta <nour.safta@medtech.tn>
2025-06-22 08:53:16 -0400

    Updated ReviewForm font styles to match app design

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:fzHleCMXUxOn4TUD2wP7i9Zn@172.21.219.7:27017";
+const mongoURI =  "mongodb://root:NT0C2gG4ULDFqk74hvs8u40Y@172.21.156.186:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
